df11a8f94024b9ea7222c2001f9d2effe11fc0cf
unknown <kylejlynch@gmail.com>
2018-06-10 13:30:30 -0500

    myapp update

--- myapp.py
+++ myapp.py
@@ -3,14 +3,18 @@ from weekly_to_excel import weeklytoexcel
 from weekly_to_plots import weeklytoplots
 from agent_weekly import agentweekly
 from agent_daily import agentdaily
+from top_perform import top_perform
+from agent_delete import agentdelete
 import os
 
 os.chdir('/home/UMRFVentures/mysite/')
 
 agentdaily()
+agentdelete()
 
 if date.today().weekday() == 0 :
     agentweekly()
+    agentdelete()
     weeklytoexcel()
     weeklytoplots()
 
